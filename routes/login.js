6329b21048943b1494768fa4d73891cd3ef8b11b
Kranki <filipkranki@gmail.com>
2024-11-14 18:02:40 +0100

    git
    naprawilem login i inne rzeczy

--- routes/login.js
+++ routes/login.js
@@ -5,14 +5,19 @@ const loginUser  = async (req, res) => {
             return res.status(400).json({ message: "Brak adresu e-mail lub hasła" });
         }
 
-        const user = await User.findOne({email});
+        const user = await User.findOne({ email });
         if (!user || !(await user.comparePassword(password))) {
             return res.status(401).json({ message: "Nieprawidłowe dane logowania" });
         }
-
-        const token = jwt.sign({id: user._id}, process.env.secret, {expiresIn: '730h'});
-        res.status(200).json({token});
+        
+        const token = jwt.sign(
+            { id: user._id, name: user.name }, 
+            process.env.secret,
+            { expiresIn: '730h' }
+        );
+        
+        res.status(200).json({ token });
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
 };
