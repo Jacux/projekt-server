fc13ae03b43e86e8c100c4cedffbd99bb11aa12a
Jacek <Jacekbialek24@gmail.com>
2024-11-14 20:30:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
-app.use("/time", (req, res) => {
-  req.next();
+app.get("/time", (req, res) => {
+  const event = new Date();
+  console.log(event.toString());
+  // Expected output: "Wed Oct 05 2011 16:48:00 GMT+0200 (CEST)"
+  // Note: your timezone may vary
+
+  console.log(event.toISOString());
+
+  res.json({ e1: event.toString(), e2: event.toISOString() });
 });
 app.post("/loginUser", loginUser);
 app.post("/createUser", createUser);
